e5f24c9977a902f3cde02b722b9fb27f5393bf24
gladkill <gladkillmusic@gmail.com>
2020-09-09 19:35:04 -0700

    fixed nav links

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import './Navbar.css';
 import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {NavLink} from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import logo from './img/gk-logo.png';
-// import logo from './img/gk-logo.png'
 function Navigation() {
   return (
     <div>
-     
-        <Nav className="justify-content-center text-white" >
+     <Navbar>
+        <Nav className="m-auto" >
           <Nav.Item>
-            <Nav.Link href="/">HOME</Nav.Link>
+            <NavLink exact to="/">HOME</NavLink>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="/about">ABOUT</Nav.Link>
+            <NavLink to="/about">ABOUT</NavLink>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="/music">MUSIC</Nav.Link>
+            <NavLink to="/music">MUSIC</NavLink>
             </Nav.Item>
           <Nav.Item>  
-            <Nav.Link href="/contact">CONTACT</Nav.Link>
+            <NavLink to="/contact">CONTACT</NavLink>
             </Nav.Item>
         </Nav>
+     </Navbar>
       <Card className="black text-white mt-5 mb-4">
         <Card.Img className="logoMain" variant="top" src={logo} />
       </Card>
