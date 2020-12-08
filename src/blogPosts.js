4e3799a3d28a5dd112589cce5525d1800542cf26
gladkill <gladkillmusic@gmail.com>
2020-12-07 18:03:49 -0800

    added more info on medusa blog post

--- src/blogPosts.js
+++ src/blogPosts.js
@@ -1,34 +1,40 @@
 const blogPosts = [
   {
-    title:"Satellite Respond: Medusa",
-    createdOn:"October 26th, 2020",
-    imgSrc:"/blogImg/sr-medusa.jpg",
-    body:"The second single from Gladkill’s project ‘Satellite Respond’ is available now, everywhere you listen to music.",
+    title: "Satellite Respond: Medusa",
+    createdOn: "October 26th, 2020",
+    imgSrc: "/blogImg/sr-medusa.jpg",
+    body:
+      "The second single from Gladkill’s project ‘Satellite Respond’ is available now, everywhere you listen to music. 'Medusa' features haunting vocoded vocals of loss,love and reconciliation mixed with evolving analog synth textures overl aid on complex percusion.",
   },
   {
-    title:"Satellite Respond: Fugue State",
-    createdOn:"September 22th, 2020",
-    imgSrc:"/blogImg/sr-fugue-state.jpg",
-    body:"The first single from Gladkill’s other project ‘Satellite Respond’ is available now, everywhere you listen to music. Live guitar, vocals, and piano meet glitchy drums, ambient soundscapes and haunting melodies to create Satellite Respond’s nuanced other-worldly sound.  Next single coming very soon, as well as the debut EP “Transmission Zero” in early 2021."
+    title: "Satellite Respond: Fugue State",
+    createdOn: "September 22th, 2020",
+    imgSrc: "/blogImg/sr-fugue-state.jpg",
+    body:
+      "The first single from Gladkill’s other project ‘Satellite Respond’ is available now, everywhere you listen to music. Live guitar, vocals, and piano meet glitchy drums, ambient soundscapes and haunting melodies to create Satellite Respond’s nuanced other-worldly sound.  Next single coming very soon, as well as the debut EP “Transmission Zero” in early 2021.",
   },
   {
-    title:"Sophrosyne Sound",
-    createdOn:"August 12th, 2020",
-    imgSrc:"/blogImg/sophro-mini-2.jpg",
-    body:"The Gladkill project is now being represented by Sophrosyne Sound. This marks the beginning of an amazing new chapter in my musical journey and I can't wait to share what the future of my music holds. Reach out to izzy@sophrosynesound.com for booking and general inquiries.", 
+    title: "Sophrosyne Sound",
+    createdOn: "August 12th, 2020",
+    imgSrc: "/blogImg/sophro-mini-2.jpg",
+    body:
+      "The Gladkill project is now being represented by Sophrosyne Sound. This marks the beginning of an amazing new chapter in my musical journey and I can't wait to share what the future of my music holds. Reach out to izzy@sophrosynesound.com for booking and general inquiries.",
   },
   {
-    title:"Welcome To The New Site.",
-    createdOn:"July 10th, 2020",
-    imgSrc:"/blogImg/gk-tree.jpg",
-    body:"Welcome to the new and improved Gladkillmusic.com, where you can find all news, releases, free music, show announcements and more. Stay tuned for new releases, exclusive merch drops, and up to date information. ", 
+    title: "Welcome To The New Site.",
+    createdOn: "July 10th, 2020",
+    imgSrc: "/blogImg/gk-tree.jpg",
+    body:
+      "Welcome to the new and improved Gladkillmusic.com, where you can find all news, releases, free music, show announcements and more. Stay tuned for new releases, exclusive merch drops, and up to date information. ",
   },
   {
-  title:"Coven Collection Now Available!",
-  createdOn:"March 20th, 2020",
-  imgSrc:"/blogImg/coven-cover-mini.jpg",
-  body:`Introducing the Coven Collection. 19 tracks from 2016-2019 that have not seen the light of day, from WiPs to mixtape-exclusives to Live-only tracks. Now available everywhere you listen to music.`, 
-}
-]
+    title: "Coven Collection Now Available!",
+    createdOn: "March 20th, 2020",
+    imgSrc: "/blogImg/coven-cover-mini.jpg",
+    body: `Introducing the Coven Collection. 19 tracks from 2016-2019 that have not seen the light of day, from WiPs to mixtape-exclusives to Live-only tracks. Now available everywhere you listen to music.`,
+  },
+];
 
-export default blogPosts
+export default blogPosts;
+
+
