9108715d5324f52f9b67a7cbf87e1246e74a1fac
gladkill <gladkillmusic@gmail.com>
2021-06-09 14:04:48 -0700

    updated footer

--- src/Footer.js
+++ src/Footer.js
@@ -7,7 +7,7 @@ function Footer(){
   return(
     <Navbar sticky="bottom" className=" pt-5 black text-white text-center flex-column">
             {/* <h6>Items</h6> */}
-      <h6> &copy;Boris Gladkikh 2020</h6>
+      <h6> &copy;Boris Gladkikh 2017-2021</h6>
 
     </Navbar>
   )
