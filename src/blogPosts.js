0a5413ba7658e8aa991e172ea9efc002daf3ad59
gladkill <gladkillmusic@gmail.com>
2021-06-09 01:00:23 -0700

    added link functionality in blog post detail

--- src/blogPosts.js
+++ src/blogPosts.js
@@ -4,6 +4,12 @@ const blogPosts = [
     createdOn: "June 2, 2021",
     imgSrc: "/blogImg/kb-flyer.jpeg",
     featured: true,
+    links: [
+      {
+        name: 'ticket link',
+        url: 'https://kickbackatthepark.eventbrite.com',
+      }
+    ],
     body: 'SoCal -  coming back  your way with a stacked lineup in July. Sound Xperiment presents: Kick Back at the Park, Featuring Yheti, Truth, Tsuruda, Jantsen, myself + Many More on July 10-11th, 2021 from 12:00pm - 10:00pm.'
     
   },
@@ -12,12 +18,36 @@ const blogPosts = [
     createdOn: "May 18, 2021",
     imgSrc: "/covers/ossuary-cover.jpg",
     featured: true,
+    links: [
+      {
+        name: 'Buy on Bandcamp',
+        url: 'https://gladkill.bandcamp.com/album/ossuary',
+      },
+      {
+        name: 'Listen on Spotify',
+        url: 'https://open.spotify.com/album/2A0O93A2o1J9mf0OovwSnf',
+      },
+    ],
     body: 'Presenting \'Ossuary\'. A collection of ten tracks written over the last several years, with influences ranging from wave, garage, grime, bass music, trap, melodic hip hop to everything in-between. This is a very special album to me and a huge cathartic experience finally releasing it into the world. Thank you for listening.'
   },
   {
     title: "Analog Whispers - Second Single From 'Ossuary'.",
     createdOn: "May 4, 2021",
     imgSrc: "/blogImg/analog-whispers-cover.jpg",
+    links: [
+      {
+        name: 'The Untz write-up',
+        url: 'https://www.theuntz.com/news/gladkill-teases-long-awaited-album-with-analog-whispers/',
+      },
+      {
+        name: 'listen on Soundcloud',
+        url: 'https://soundcloud.com/theuntz/gladkill-analog-whispers?fbclid=IwAR0tctq_ayPhV8vmhXaztKaNJPR7iIwRwDF7lcoD9g-YrOEEBWxOm1P-MsE',
+      },
+      {
+        name: 'Buy on Bandcamp',
+        url: 'https://gladkill.bandcamp.com/track/analog-whispers',
+      },
+    ],
     featured: true,
     body: 'Presenting \'Analog Whispers\', the second single off of \'Ossuary\'. Special shout out to The Untz for premiering this single - it is definitely one of my favorites off of the album. You  can find their awesome write-up about it below. '
 
@@ -28,6 +58,12 @@ const blogPosts = [
     imgSrc: "/blogImg/ashes-cover.jpg",
     featured: true,
     body: 'The first single off of Gladkill\'s long awaited album \'Ossuary\', \'Ashes\' is a return to form, setting the mood with haunting vocal ambience intertwined with melancholy piano and deep rich bass. \'Ossuary\' will be available everywhere you stream music on May 18, 2021.'
+  },
+  {
+    title: "Gladkill Is Now On Audius.",
+    createdOn: "May 4, 2021",
+    imgSrc: "/blogImg/audius-admat.jpg",
+    body: 'Gladkill is now on Audius! Come check out my latest compilation, \'Coven Collection\', as well as future upcoming tunes on this platform.',
   },
   {
     title: "Satellite Respond: Medusa",
