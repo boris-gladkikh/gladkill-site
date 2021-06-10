61ced82b3788ddd965c2582d04b23a18088b56fb
gladkill <gladkillmusic@gmail.com>
2021-06-09 18:18:27 -0700

    fixed carousel featured to properly show text and dim opacity on hover

--- src/Featured.js
+++ src/Featured.js
@@ -2,24 +2,25 @@ import React from "react";
 import blogPosts from "./blogPosts";
 import Carousel from "react-bootstrap/Carousel";
 import { NavLink } from "react-router-dom";
-import './Featured.css'
+import "./Featured.css";
 
 function Featured() {
   let featuredPosts = blogPosts.filter((post) => post.featured);
   let posts = featuredPosts.map((post) => {
     return (
       <Carousel.Item>
+        <div className="carousel-interior">
+
+          <h3 className="carousel-text">{post.title.toUpperCase()}</h3>
           <NavLink exact to={`/blog/${post.title}`}>
-          <div className="carousel-text">
-            <h5>{post.title}</h5>
-          </div>
-          <img
-            className="carousel-img d-block w-100 img-fluid"
-            src={post.imgSrc}
-            alt={post.title}
-          />
-      </NavLink>
-        </Carousel.Item>
+            <img
+              className="carousel-img d-block w-100 img-fluid"
+              src={post.imgSrc}
+              alt={post.title}
+            />
+          </NavLink>
+        </div>
+      </Carousel.Item>
     );
   });
 
