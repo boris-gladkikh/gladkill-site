ed7e6e0d1f870992d4461f9cdc40dd937643058e
gladkill <gladkillmusic@gmail.com>
2021-06-10 18:07:42 -0700

    smaller 404

--- src/NotFound.js
+++ src/NotFound.js
@@ -6,7 +6,10 @@ import skull404 from './img/404-skull-design.png';
 function NotFound() {
   return (
     <div className="container text-center">
-      <img src={skull404} alt="custom 404"/>
+      <div>
+
+      <img className="not-found-img" src={skull404} alt="custom 404"/>
+      </div>
       <p id="not-found-text">What you seek does not exist.</p>
       <NavLink exact to="/">
         <p id="blog-detail-back-btn">GO HOME</p>
