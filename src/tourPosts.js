ade2e1a7084e557af0c8e66a912cf22ac59666ec
gladkill <gladkillmusic@gmail.com>
2023-02-28 14:45:27 -0800

    updated tour posts

--- src/tourPosts.js
+++ src/tourPosts.js
@@ -1,9 +1,9 @@
  const tourPosts = [
    {
-     date:'December 3, 2022',
-     venue: 'Pantheon',
-     location: 'Seattle, WA, United States ',
-     tickets: 'https://www.eventbrite.com/e/pantheon-tickets-386517553407',
+     date:'March 17, 2023',
+     venue: 'Wormhole Presents - Shenanigans',
+     location: 'Los Angeles, CA, United States ',
+     tickets: 'https://www.eventbrite.com/e/317-dtla-shenanigans-ft-wormhole-music-group-bass-waffles-tickets-557303799667?fbclid=IwAR3OvsnJuUJGy8OeW3RVBp9qm2n09Px0kx9fp1mKo76BsLrFknCzafMAR58',
    },
 
  ];
