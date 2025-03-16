8f0c320786b5c8848bd9b92df87eff8714ef2f36
gladkill <gladkillmusic@gmail.com>
2025-03-16 15:05:02 -0700

    update blog post detail page

--- src/Home.js
+++ src/Home.js
@@ -12,8 +12,9 @@ function Home() {
 
   return (
     <div className="mt-5 black container text-center"> 
+      {/* <h4 className="my-3 text-white">FEATURED</h4>
+      <Featured /> */}
       <h5 className="my-3 text-white">NEWS</h5>
-      {/* <Featured /> */}
       <BlogList />
     </div>
   )
